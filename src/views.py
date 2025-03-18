dbfa5359a1ad64643751ed1d8ea9f9152332d23b
JavaPro91 <ivan-don4encko@ya.ru>
2025-03-18 20:53:01 +0300

    Основная функция вызова других(вспомогательных) функций

--- src/views.py
+++ src/views.py
@@ -1,4 +1,25 @@
+from src.utils import (
+    currency_rates,
+    main_cards,
+    read_finance_excel_operation,
+    top_transactions,
+    user_stocks,
+    welcome_text,
+)
 
-def page_main(date: str):
-    datetime = "YYYY-MM-DD HH:MM:SS"
-    pass
+
+def page_main(date: str) -> dict:
+    """
+    Функция главной страницы возвращает основную информацию.
+    :param date: Входящая дата.
+    :return: Json объект содержащий информацию.
+    """
+    struct_file_json = read_finance_excel_operation()
+    json_response = {
+        "greeting": welcome_text(date),
+        "cards": main_cards(struct_file_json),
+        "top_transactions": top_transactions(struct_file_json),
+        "currency_rates": currency_rates(),
+        "stock_prices": user_stocks(),
+    }
+    return json_response
