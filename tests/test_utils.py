0a8279a35ec869e6a3e2cc703ae00db5d5dcbdea
JavaPro91 <ivan-don4encko@ya.ru>
2025-03-18 23:38:48 +0300

    Правка проекта, добавление фильтрации по дате - период

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,16 @@
+import datetime
 import os
 from typing import Any
 from unittest.mock import patch
 
 import pytest
+import requests
 
 from src.utils import (
     currency_rates,
     get_api_currency,
     get_api_stocks,
+    get_period_date,
     get_user_settings,
     main_cards,
     read_finance_excel_operation,
@@ -18,15 +21,32 @@ from src.utils import (
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+def test_get_period_date() -> None:
+    """
+    [Тест] Функция для считывания финансовых операций из Excel выдает список словарей с транзакциями.
+    """
+    assert get_period_date("2025-03-12 22:53:10") == (
+        datetime.datetime(2025, 3, 12, 22, 53, 10),
+        datetime.datetime(2025, 3, 1, 22, 53, 10),
+    )
+
+
 def test_read_finance_excel_operation() -> None:
     """
     [Тест] Функция для считывания финансовых операций из Excel выдает список словарей с транзакциями.
     """
-    success_test = read_finance_excel_operation(ROOT_DIR + "/data/operations.xlsx")
+    dates = (datetime.datetime(2021, 12, 27, 22, 53, 10), datetime.datetime(2021, 12, 1, 22, 53, 10))
+    success_test = read_finance_excel_operation(dates, ROOT_DIR + "/data/operations.xlsx")
     assert isinstance(success_test, list)
 
     with pytest.raises(ValueError):
-        read_finance_excel_operation("")
+        dates = (datetime.datetime(2021, 12, 27, 22, 53, 10), datetime.datetime(2021, 12, 1, 22, 53, 10))
+        read_finance_excel_operation(dates, "")
+
+    with patch("pandas.read_excel") as read_pd:
+        read_pd.return_value = "test_data"
+        with pytest.raises(Exception):
+            read_finance_excel_operation(dates, "")
 
 
 @pytest.mark.parametrize(
@@ -51,6 +71,9 @@ def test_main_cards(excel_data: list[dict]) -> None:
     """
     data = main_cards(excel_data)
     assert data == [{"cashback": 0.0, "last_digits": "7197", "total_spent": 8023.92}]
+
+    with pytest.raises(Exception):
+        main_cards([{}])
 
 
 def test_top_transactions(excel_data: list[dict]) -> None:
@@ -82,6 +105,14 @@ def test_get_api_currency(requests_mock: Any) -> None:
     data = get_api_currency("USD")
     assert data == 1.887787
 
+    requests_mock.return_value.status_code = 500
+    data = get_api_currency("USD")
+    assert data == 0
+
+    requests_mock.side_effect = requests.exceptions.ReadTimeout
+    with pytest.raises(requests.exceptions.ReadTimeout):
+        get_api_currency("USD")
+
 
 @patch("requests.get")
 def test_get_api_stocks(requests_mock: Any) -> None:
@@ -92,6 +123,15 @@ def test_get_api_stocks(requests_mock: Any) -> None:
     requests_mock.return_value.json.return_value = {"Global Quote": {"05. price": 32.223}}
     data = get_api_stocks("USD")
     assert data == 32.223
+
+    requests_mock.return_value.status_code = 200
+    requests_mock.return_value.json.return_value = {"Global Quotes": {"05. price": 32.223}}
+    with pytest.raises(Exception):
+        get_api_stocks("USD")
+
+    requests_mock.return_value.status_code = 500
+    data = get_api_stocks("USD")
+    assert data == 0
 
 
 @patch("requests.get")
@@ -123,3 +163,8 @@ def test_get_user_settings() -> None:
     """
     data = get_user_settings()
     assert data == {"user_currencies": ["USD", "EUR"], "user_stocks": ["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA"]}
+
+    with patch("builtins.open") as f_open:
+        f_open("fake_file")
+        with pytest.raises(Exception):
+            get_user_settings()
