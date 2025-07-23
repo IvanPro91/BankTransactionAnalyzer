0448036095eb4b5258418f023e3e68a870095e75
IvanPro91 <ivan-don4encko@yandex.ru>
2025-07-23 22:44:03 +0300

    Внесение изменений, улучшения

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from src.views import page_main
+from src.views import page_main, page_notification
 
 if __name__ == "__main__":
-    data = page_main("2021-12-27 08:00:23")
-    print(data)
+    # data = page_main("2021-12-25 23:00:23")
+    page_notify = page_notification("2021-12-25")
+    print(page_notify)
--- src/views.py
+++ src/views.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+from pandas import DataFrame
+
 from src.utils import (
     currency_rates,
     get_period_date,
@@ -6,6 +10,9 @@ from src.utils import (
     top_transactions,
     user_stocks,
     welcome_text,
+    notifications,
+    expenses_total_amount,
+    income_total_amount,
 )
 
 
@@ -15,13 +22,36 @@ def page_main(date: str) -> dict:
     :param date: Входящая дата.
     :return: Json объект содержащий информацию.
     """
-    period_date = get_period_date(date)
-    struct_file_json = read_finance_excel_operation(period_date)
-    json_response = {
+    period_date: tuple[datetime, datetime] = get_period_date(date)
+    struct_file_json: DataFrame = read_finance_excel_operation(period_date)
+
+    data = {
         "greeting": welcome_text(date),
         "cards": main_cards(struct_file_json),
         "top_transactions": top_transactions(struct_file_json),
         "currency_rates": currency_rates(),
         "stock_prices": user_stocks(),
     }
-    return json_response
+    return data
+
+
+def page_notification(date: str):
+    end_day: datetime = datetime.strptime(date, "%Y-%m-%d")
+    start_day: datetime = notifications(date, "W")
+    period_date: tuple[datetime, datetime] = (start_day, end_day)
+    struct_file_json: DataFrame = read_finance_excel_operation(period_date)
+    print(expenses_total_amount(struct_file_json))
+    data = {
+        "expenses": {
+            "total_amount": expenses_total_amount(struct_file_json),
+            "main": [{"category": "Супермаркеты", "amount": 17319}],
+            "transfers_and_cash": [{"category": "Наличные", "amount": 500}],
+        },
+        "income": {
+            "total_amount": income_total_amount(struct_file_json),
+            "main": [{"category": "Пополнение_BANK007", "amount": 33000}],
+        },
+        "currency_rates": currency_rates(),
+        "stock_prices": user_stocks(),
+    }
+    return data
